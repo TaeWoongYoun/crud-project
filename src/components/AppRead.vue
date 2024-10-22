f627fea2155e9dc6cbe7f0820f9ead82a46cc66a
xodnd0507 <24457545yong@gmail.com>
2024-10-22 13:45:20 +0900

    feat : Create read file

---
+++ src/components/AppRead.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>언어</th>
+                    <th>분야</th>
+                    <th>번호</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(value, index) in data" :key="index">
+                    <td>{{ value.writer }}</td>
+                    <td>{{ value.title }}</td>
+                    <td>{{ value.content }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import data from '@/data/index'
+
+export default {
+    name: 'AppRead',
+    data() {
+        return{
+            data: data
+        }
+    }
+}
+</script>
