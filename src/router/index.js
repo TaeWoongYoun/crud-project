759ae1631d390ef1fa0780f5ad3686a2e32f25cd
xodnd0507 <24457545yong@gmail.com>
2024-10-22 13:45:38 +0900

    test : Router js

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import AppRead from '@/components/AppRead.vue';
+import AppCreate from '@/components/AppCreate.vue';
+
+const routes = [
+    {
+        path: '/read',
+        name: 'AppRead',
+        component: AppRead
+    },
+    {
+        path: '/create',
+        name: 'AppCreate',
+        component: AppCreate
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
